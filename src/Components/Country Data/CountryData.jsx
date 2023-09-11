f587e1248b4c04677750d55d025f2e5161310a67
gmlincoln <maula03@gmail.com>
2023-09-11 15:05:51 +0600

    updated-classname

--- src/Components/Country Data/CountryData.jsx
+++ src/Components/Country Data/CountryData.jsx
@@ -1,11 +1,11 @@
 const CountryData = (props) => {
     const { country } = props;
-    const countryOfficialStyel = {
+    const countryOfficialStyle = {
         background: 'yellow'
     }
     return (
         <div>
-            <h4>Official Name: <span style={countryOfficialStyel}>{country?.name?.official}</span></h4>
+            <h4>Official Name: <span style={countryOfficialStyle}>{country?.name?.official}</span></h4>
         </div>
     );
 };
